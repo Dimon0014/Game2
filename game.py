8b28bf054b505283c5a4b7eb88f614d178441ebe
Dimon0014 <toropov0014@mail.ru>
2017-05-10 15:41:41 +0300

    Game converted to class

--- game.py
+++ game.py
@@ -1,10 +1,17 @@
-import arcade as arcade
+import arcade as arcad
+
+class MyGame(arcad.Window):
+   def on_draw(self):
+       arcad.start_render()
+       arcad.draw_text('Hello', 100, 100, arcad.color.WHITE)
+
+       ("""arcade.open_window(600, 600, 'My Game' )
+       arcade.finish_render()""")
+
+
 def main():
-    arcade.open_window(600, 600, 'My Game' )
-    arcade.start_render()
-    arcade.draw_text('Hello',100,100, arcade.color.WHITE)
-    arcade.finish_render()
-    arcade.run()
+    MyGame(600,600,'My Game')
+    arcad.run()
 
 if __name__ == '__main__':
     main()
