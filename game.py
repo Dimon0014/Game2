e4df922178f42c04d353c1bf31c8cc65672e1c03
Dimon0014 <toropov0014@mail.ru>
2017-05-11 02:29:12 +0300

    Player sprite

--- game.py
+++ game.py
@@ -8,13 +8,19 @@ class MyGame(arcad.Window):
        super().__init__(width, height, title)
        arcad.set_background_color(arcad.color.AMAZON)
        self.score = 0
+       self.all_sprites_list = arcad.SpriteList()
+       self.player_sprite = arcad.Sprite('character2.png', 0.7)
+       self.player_sprite.center_x = 50
+       self.player_sprite.center_y = 50
+       self.all_sprites_list.append(self.player_sprite)
    def on_draw(self):
        arcad.start_render()
        output = f'Score: {self.score:02d}'
        arcad.draw_text(output, 100, 100, arcad.color.WHITE)
+       self.all_sprites_list.draw()
    def update(self, delta_time):
        self.score += 1
-
+       self.all_sprites_list.update()
 
 def main():
     MyGame(600,600,'My Game_game')
