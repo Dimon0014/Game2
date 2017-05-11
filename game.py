2b37c3bd1a1b0286c6b94fbd430bb88a137ab866
Dimon0014 <toropov0014@mail.ru>
2017-05-11 03:22:05 +0300

    Coin sprites.

--- game.py
+++ game.py
@@ -8,11 +8,21 @@ class MyGame(arcad.Window):
        super().__init__(width, height, title)
        arcad.set_background_color(arcad.color.AMAZON)
        self.score = 0
+
        self.all_sprites_list = arcad.SpriteList()
        self.player_sprite = arcad.Sprite('character2.png', 0.7)
        self.player_sprite.center_x = 50
        self.player_sprite.center_y = 50
        self.all_sprites_list.append(self.player_sprite)
+
+       self.coin_list = arcad.SpriteList()
+       for i in range(50):
+          coin = arcad.Sprite('coin.png', 0.15)
+          coin.center_x = random.randrange(600)
+          coin.center_y = random.randrange(600)
+          self.all_sprites_list.append(coin) # это все в цикле делается, в том числе
+                                             # coin объявляется
+          self.coin_list.append(coin)
    def on_draw(self):
        arcad.start_render()
        output = f'Score: {self.score:02d}'
